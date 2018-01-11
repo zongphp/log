2171d87a47dc88da5c21d4a8aa65a7a1be1acb3d
zmingzhi <zmingzhi@yeah.net>
2018-01-11 16:48:17 +0800

    2.0

--- src/LogProvider.php
+++ src/LogProvider.php
@@ -3,16 +3,19 @@ namespace zongphp\log;
 
 use zongphp\framework\build\Provider;
 
-class LogProvider extends Provider {
-	//延迟加载
-	public $defer = true;
+class LogProvider extends Provider
+{
+    //延迟加载
+    public $defer = false;
 
-	public function boot() {
-	}
+    public function boot()
+    {
+    }
 
-	public function register() {
-		$this->app->single( 'Log', function (  ) {
-			return new Log();
-		} );
-	}
+    public function register()
+    {
+        $this->app->single('Log', function () {
+            return Log::single();
+        });
+    }
 }
